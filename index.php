a41d52bf09a3bce790e71a75f552498ab15ab577
Anuki-UCSC <2018cs030@stu.ucsc.cmb.ac.lk>
2023-05-10 23:58:50 +0530

    doctor search

--- index.php
+++ index.php
@@ -2,232 +2,228 @@
 session_start();
 //error_reporting(0);
 include('views/includes/dbconnection.php');
-    if(isset($_POST['submit']))
-  {
- $name=$_POST['name'];
-  $mobnum=$_POST['phone'];
- $email=$_POST['email'];
- $appdate=$_POST['date'];
- $aaptime=$_POST['time'];
- $specialization=$_POST['specialization'];
-  $doctorlist=$_POST['doctorlist'];
- $message=$_POST['message'];
- $aptnumber=mt_rand(100000000, 999999999);
- $cdate=date('Y-m-d');
+if (isset($_POST['submit'])) {
+    $name = $_POST['name'];
+    $mobnum = $_POST['phone'];
+    $email = $_POST['email'];
+    $appdate = $_POST['date'];
+    $aaptime = $_POST['time'];
+    $specialization = $_POST['specialization'];
+    $doctorlist = $_POST['doctorlist'];
+    $message = $_POST['message'];
+    $aptnumber = mt_rand(100000000, 999999999);
+    $cdate = date('Y-m-d');
 
-if($appdate<=$cdate){
-       echo '<script>alert("Appointment date must be greater than todays date")</script>';
-} else {
-$sql="insert into tblappointment(AppointmentNumber,Name,MobileNumber,Email,AppointmentDate,AppointmentTime,Specialization,Doctor,Message)values(:aptnumber,:name,:mobnum,:email,:appdate,:aaptime,:specialization,:doctorlist,:message)";
-$query=$dbh->prepare($sql);
-$query->bindParam(':aptnumber',$aptnumber,PDO::PARAM_STR);
-$query->bindParam(':name',$name,PDO::PARAM_STR);
-$query->bindParam(':mobnum',$mobnum,PDO::PARAM_STR);
-$query->bindParam(':email',$email,PDO::PARAM_STR);
-$query->bindParam(':appdate',$appdate,PDO::PARAM_STR);
-$query->bindParam(':aaptime',$aaptime,PDO::PARAM_STR);
-$query->bindParam(':specialization',$specialization,PDO::PARAM_STR);
-$query->bindParam(':doctorlist',$doctorlist,PDO::PARAM_STR);
-$query->bindParam(':message',$message,PDO::PARAM_STR);
+    if ($appdate <= $cdate) {
+        echo '<script>alert("Appointment date must be greater than todays date")</script>';
+    } else {
+        $sql = "insert into tblappointment(AppointmentNumber,Name,MobileNumber,Email,AppointmentDate,AppointmentTime,Specialization,Doctor,Message)values(:aptnumber,:name,:mobnum,:email,:appdate,:aaptime,:specialization,:doctorlist,:message)";
+        $query = $dbh->prepare($sql);
+        $query->bindParam(':aptnumber', $aptnumber, PDO::PARAM_STR);
+        $query->bindParam(':name', $name, PDO::PARAM_STR);
+        $query->bindParam(':mobnum', $mobnum, PDO::PARAM_STR);
+        $query->bindParam(':email', $email, PDO::PARAM_STR);
+        $query->bindParam(':appdate', $appdate, PDO::PARAM_STR);
+        $query->bindParam(':aaptime', $aaptime, PDO::PARAM_STR);
+        $query->bindParam(':specialization', $specialization, PDO::PARAM_STR);
+        $query->bindParam(':doctorlist', $doctorlist, PDO::PARAM_STR);
+        $query->bindParam(':message', $message, PDO::PARAM_STR);
 
- $query->execute();
-   $LastInsertId=$dbh->lastInsertId();
-   if ($LastInsertId>0) {
-    echo '<script>alert("Your Appointment Request Has Been Send. We Will Contact You Soon")</script>';
-echo "<script>window.location.href ='index.php'</script>";
-  }
-  else
-    {
-         echo '<script>alert("Something Went Wrong. Please try again")</script>';
+        $query->execute();
+        $LastInsertId = $dbh->lastInsertId();
+        if ($LastInsertId > 0) {
+            echo '<script>alert("Your Appointment Request Has Been Send. We Will Contact You Soon")</script>';
+            echo "<script>window.location.href ='index.php'</script>";
+        } else {
+            echo '<script>alert("Something Went Wrong. Please try again")</script>';
+        }
     }
-}
 }
 ?>
 <!doctype html>
 <html lang="en">
-    <head>
-        <title>Doctor Appointment Management System || Home Page</title>
 
-        <!-- CSS FILES -->        
-        <link rel="preconnect" href="https://fonts.googleapis.com">
-        
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-        
-        <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;600;700&display=swap" rel="stylesheet">
+<head>
+    <title>Doctor Appointment Management System || Home Page</title>
 
-        <link href="css/bootstrap.min.css" rel="stylesheet">
+    <!-- CSS FILES -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
 
-        <link href="css/bootstrap-icons.css" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 
-        <link href="css/owl.carousel.min.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;600;700&display=swap" rel="stylesheet">
 
-        <link href="css/owl.theme.default.min.css" rel="stylesheet">
+    <link href="css/bootstrap.min.css" rel="stylesheet">
 
-        <link href="css/templatemo-medic-care.css" rel="stylesheet">
-        <script>
-// function getdoctors(val) {
-//   //  alert(val);
-// $.ajax({
+    <link href="css/bootstrap-icons.css" rel="stylesheet">
 
-// type: "POST",
-// url: "get_doctors.php",
-// data:'sp_id='+val,
-// success: function(data){
-// $("#doctorlist").html(data);
-// }
-// });
-// }
-</script>
-    </head>
-    
-    <body id="top">
-    
-        <main>
+    <link href="css/owl.carousel.min.css" rel="stylesheet">
 
-            <?php include_once('includes/header.php');?>
+    <link href="css/owl.theme.default.min.css" rel="stylesheet">
 
-            <section class="hero" id="hero">
-                <div class="container">
-                    <div class="row">
+    <link href="css/templatemo-medic-care.css" rel="stylesheet">
+    <script>
+    </script>
+</head>
 
-                        <div class="col-12">
-                            <div id="myCarousel" class="carousel slide carousel-fade" data-bs-ride="carousel">
-                                <div class="carousel-inner">
-                                    <div class="carousel-item active">
-                                        <img src="images/slider/portrait-successful-mid-adult-doctor-with-crossed-arms.jpg" class="img-fluid" alt="">
-                                    </div>
+<body id="top">
 
-                                    <div class="carousel-item">
-                                        <img src="images/slider/young-asian-female-dentist-white-coat-posing-clinic-equipment.jpg" class="img-fluid" alt="">
-                                    </div>
+    <main>
 
-                                    <div class="carousel-item">
-                                        <img src="images/slider/doctor-s-hand-holding-stethoscope-closeup.jpg" class="img-fluid" alt="">
-                                    </div>
+        <?php include_once('includes/header.php'); ?>
+
+        <section class="hero" id="hero">
+            <div class="container">
+                <div class="row">
+
+                    <div class="col-12">
+                        <div id="myCarousel" class="carousel slide carousel-fade" data-bs-ride="carousel">
+                            <div class="carousel-inner">
+                                <div class="carousel-item active">
+                                    <img src="images/slider/portrait-successful-mid-adult-doctor-with-crossed-arms.jpg" class="img-fluid" alt="">
+                                </div>
+
+                                <div class="carousel-item">
+                                    <img src="images/slider/young-asian-female-dentist-white-coat-posing-clinic-equipment.jpg" class="img-fluid" alt="">
+                                </div>
+
+                                <div class="carousel-item">
+                                    <img src="images/slider/doctor-s-hand-holding-stethoscope-closeup.jpg" class="img-fluid" alt="">
                                 </div>
                             </div>
-
                         </div>
 
                     </div>
+
                 </div>
-            </section>
+            </div>
+        </section>
 
-            <section class="section-padding" id="about">
-                <div class="container">
-                    <div class="row">
+        <section class="section-padding" id="about">
+            <div class="container">
+                <div class="row">
 
-                        <div class="col-lg-6 col-md-6 col-12">
-                            <h2 class="mb-lg-3 mb-3">About Us</h2>
+                    <div class="col-lg-6 col-md-6 col-12">
+                        <h2 class="mb-lg-3 mb-3">About Us</h2>
 
-                            <p><div><font color="#202124" face="arial, sans-serif"><b>Our mission declares our purpose of existence as a company and our objectives.</b></font></div><div><font color="#202124" face="arial, sans-serif"><b><br></b></font></div><div><font color="#202124" face="arial, sans-serif"><b>To give every customer much more than what he/she asks for in terms of quality, selection, value for money and customer service, by understanding local tastes and preferences and innovating constantly to eventually provide an unmatched experience in jewellery shopping.</b></font></div></p>
+                        <p>
+                        <div>
+                            <font color="#202124" face="arial, sans-serif"><b>Our mission declares our purpose of existence as a company and our objectives.</b></font>
                         </div>
-
-                        <div class="col-lg-4 col-md-5 col-12 mx-auto">
-                            <div class="featured-circle bg-white shadow-lg d-flex justify-content-center align-items-center">
-                                <p class="featured-text"><span class="featured-number">12</span> Years<br> of Experiences</p>
-                            </div>
+                        <div>
+                            <font color="#202124" face="arial, sans-serif"><b><br></b></font>
                         </div>
-
+                        <div>
+                            <font color="#202124" face="arial, sans-serif"><b>To give every customer much more than what he/she asks for in terms of quality, selection, value for money and customer service, by understanding local tastes and preferences and innovating constantly to eventually provide an unmatched experience in jewellery shopping.</b></font>
+                        </div>
+                        </p>
                     </div>
-                </div>
-            </section>
 
-            <section class="gallery">
-                <div class="container">
-                    <div class="row">
-
-                        <div class="col-lg-6 col-6 ps-0">
-                            <img src="images/gallery/medium-shot-man-getting-vaccine.jpg" class="img-fluid galleryImage" alt="get a vaccine" title="get a vaccine for yourself">
+                    <div class="col-lg-4 col-md-5 col-12 mx-auto">
+                        <div class="featured-circle bg-white shadow-lg d-flex justify-content-center align-items-center">
+                            <p class="featured-text"><span class="featured-number">12</span> Years<br> of Experiences</p>
                         </div>
-
-                        <div class="col-lg-6 col-6 pe-0">
-                            <img src="images/gallery/female-doctor-with-presenting-hand-gesture.jpg" class="img-fluid galleryImage" alt="wear a mask" title="wear a mask to protect yourself">
-                        </div>
-
                     </div>
+
                 </div>
-            </section>
+            </div>
+        </section>
 
-            
+        <section class="gallery">
+            <div class="container">
+                <div class="row">
 
-            
+                    <div class="col-lg-6 col-6 ps-0">
+                        <img src="images/gallery/medium-shot-man-getting-vaccine.jpg" class="img-fluid galleryImage" alt="get a vaccine" title="get a vaccine for yourself">
+                    </div>
 
-            <section class="section-padding" id="booking">
-                <div class="container">
-                    <div class="row">
-                    
-                        <div class="col-lg-8 col-12 mx-auto">
-                            <div class="booking-form">
-                                
-                                <h2 class="text-center mb-lg-3 mb-2">Book an appointment</h2>
-                            
-                                <form role="form" method="post">
-                                    <div class="row">
-                                        <div class="col-lg-6 col-12">
-                                            <input type="text" name="name" id="name" class="form-control" placeholder="Full name" required='true'>
-                                        </div>
+                    <div class="col-lg-6 col-6 pe-0">
+                        <img src="images/gallery/female-doctor-with-presenting-hand-gesture.jpg" class="img-fluid galleryImage" alt="wear a mask" title="wear a mask to protect yourself">
+                    </div>
 
-                                        <div class="col-lg-6 col-12">
-                                            <input type="email" name="email" id="email" pattern="[^ @]*@[^ @]*" class="form-control" placeholder="Email address" required='true'>
-                                        </div>
-                                   
-                                        <div class="col-lg-6 col-12">
-                                            <input type="telephone" name="phone" id="phone" class="form-control" placeholder="Enter Phone Number" maxlength="10">
-                                        </div>
-
-                                        <div class="col-lg-6 col-12">
-                                            <input type="date" name="date" id="date" value="" class="form-control">
-                                            
-                                        </div>
-
-                                            <div class="col-lg-6 col-12">
-                                            <input type="time" name="time" id="time" value="" class="form-control">
-                                            
-                                        </div>
-
-    <div class="col-lg-6 col-12">
-<select onChange="getdoctors(this.value);"  name="specialization" id="specialization" class="form-control" required>
-<option value="">Select specialization</option>
-<option value="heart">heart</option>
-<!-- <option value="<?php // echo $row['ID'];?>"><?php //echo $row['Specialization'];?></option> -->
-<!--- Fetching States--->
-</select>
-</div>
-
-
-    <div class="col-lg-6 col-12">
-<select name="doctorlist" id="doctorlist" class="form-control">
-<option value="">Select Doctor</option>
-</select>
-</div>
+                </div>
+            </div>
+        </section>
 
 
 
-                                        <div class="col-12">
-                                            <textarea class="form-control" rows="5" id="message" name="message" placeholder="Additional Message"></textarea>
-                                        </div>
 
-                                        <div class="col-lg-3 col-md-4 col-6 mx-auto">
-                                        <?php //echo $_SESSION?>
-                                            <button type="submit" class="form-control" name="submit" id="submit-button">Book Now</button>
-                                        </div>
+
+        <section class="section-padding" id="booking">
+            <div class="container">
+                <div class="row">
+
+                    <div class="col-lg-8 col-12 mx-auto">
+                        <div class="booking-form">
+
+                            <h2 class="text-center mb-lg-3 mb-2">Book an appointment</h2>
+
+                            <form role="form" method="post">
+                                <div class="row">
+                                    <div class="col-lg-6 col-12">
+                                        <input type="text" name="name" id="name" class="form-control" placeholder="Full name" required='true'>
                                     </div>
-                                </form>
 
-                            </div>
+                                    <div class="col-lg-6 col-12">
+                                        <input type="email" name="email" id="email" pattern="[^ @]*@[^ @]*" class="form-control" placeholder="Email address" required='true'>
+                                    </div>
+
+                                    <div class="col-lg-6 col-12">
+                                        <input type="telephone" name="phone" id="phone" class="form-control" placeholder="Enter Phone Number" maxlength="10">
+                                    </div>
+
+                                    <div class="col-lg-6 col-12">
+                                        <input type="date" name="date" id="date" value="" class="form-control">
+
+                                    </div>
+
+                                    <div class="col-lg-6 col-12">
+                                        <input type="time" name="time" id="time" value="" class="form-control">
+
+                                    </div>
+
+                                    <div class="col-lg-6 col-12">
+                                        <select onChange="getdoctors(this.value);" name="specialization" id="specialization" class="form-control" required>
+                                            <option value="">Select specialization</option>
+                                            <option value="heart">heart</option>
+                                        </select>
+                                    </div>
+
+
+                                    <div class="col-lg-6 col-12">
+                                        <select name="doctorlist" id="doctorlist" class="form-control">
+                                            <option value="">Select Doctor</option>
+                                        </select>
+                                    </div>
+
+
+
+                                    <div class="col-12">
+                                        <textarea class="form-control" rows="5" id="message" name="message" placeholder="Additional Message"></textarea>
+                                    </div>
+
+                                    <div class="col-lg-3 col-md-4 col-6 mx-auto">
+                                        <?php //echo $_SESSION
+                                        ?>
+                                        <button type="submit" class="form-control" name="submit" id="submit-button">Book Now</button>
+                                    </div>
+                                </div>
+                            </form>
+
                         </div>
-
                     </div>
+
                 </div>
-            </section>
-        </main>
-        <?php include_once('includes/footer.php');?>
-        <!-- JAVASCRIPT FILES -->
-        <script src="js/jquery.min.js"></script>
-        <script src="js/bootstrap.bundle.min.js"></script>
-        <script src="js/owl.carousel.min.js"></script>
-        <script src="js/scrollspy.min.js"></script>
-        <script src="js/custom.js"></script>
-    </body>
+            </div>
+        </section>
+    </main>
+    <?php include_once('includes/footer.php'); ?>
+    <!-- JAVASCRIPT FILES -->
+    <script src="js/jquery.min.js"></script>
+    <script src="js/bootstrap.bundle.min.js"></script>
+    <script src="js/owl.carousel.min.js"></script>
+    <script src="js/scrollspy.min.js"></script>
+    <script src="js/custom.js"></script>
+</body>
+
 </html>

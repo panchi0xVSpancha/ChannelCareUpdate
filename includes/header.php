a8455dbbf4bf034120b4a174317ac9beff6c6750
Anuki-UCSC <2018cs030@stu.ucsc.cmb.ac.lk>
2023-05-10 23:59:50 +0530

    format change

--- includes/header.php
+++ includes/header.php
@@ -1,47 +1,47 @@
 <nav class="navbar navbar-expand-lg bg-light fixed-top shadow-lg">
-                <div class="container">
-                    <a class="navbar-brand mx-auto d-lg-none" href="index.php">
-                        Doctor Appointment
-                        <strong class="d-block">Management System</strong>
-                    </a>
+    <div class="container">
+        <a class="navbar-brand mx-auto d-lg-none" href="index.php">
+            Doctor Appointment
+            <strong class="d-block">Management System</strong>
+        </a>
 
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
-                    </button>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
 
-                    <div class="collapse navbar-collapse" id="navbarNav">
-                        <ul class="navbar-nav mx-auto">
-                            <li class="nav-item active">
-                                <a class="nav-link" href="index.php">Home</a>
-                            </li>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav mx-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="index.php">Home</a>
+                </li>
 
-                            <li class="nav-item">
-                                <a class="nav-link" href="#about">About</a>
-                            </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#about">About</a>
+                </li>
 
-                           
 
-                            <a class="navbar-brand d-none d-lg-block" href="index.php">
-                                Doctor Appointment
-                                <strong class="d-block">Management System</strong>
-                            </a>
 
-                            <li class="nav-item">
-                                <a class="nav-link" href="check-appointment.php">Check Appointment</a>
-                            </li>
+                <a class="navbar-brand d-none d-lg-block" href="index.php">
+                    Doctor Appointment
+                    <strong class="d-block">Management System</strong>
+                </a>
 
-                            <li class="nav-item">
-                                <a class="nav-link" href="#booking">Booking</a>
-                            </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="check-appointment.php">Check Appointment</a>
+                </li>
 
-                            <li class="nav-item">
-                                <a class="nav-link" href="#contact">Contact</a>
-                            </li>
-                             <li class="nav-item active">
-                                <a class="nav-link" href="views/login.php">Login</a>
-                            </li>
-                        </ul>
-                    </div>
+                <li class="nav-item">
+                    <a class="nav-link" href="#booking">Booking</a>
+                </li>
 
-                </div>
-            </nav>
+                <li class="nav-item">
+                    <a class="nav-link" href="#contact">Contact</a>
+                </li>
+                <li class="nav-item active">
+                    <a class="nav-link" href="views/login.php">Login</a>
+                </li>
+            </ul>
+        </div>
+
+    </div>
+</nav>
